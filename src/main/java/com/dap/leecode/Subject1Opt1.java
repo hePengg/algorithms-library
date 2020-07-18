5c565e22582c04a09af7e7fd502637513a1245c8
hep <377909@qq.com>
2020-07-18 17:07:27 +0800

    两数之和新增优化解

--- src/main/java/com/dap/leecode/Subject1Opt1.java
+++ src/main/java/com/dap/leecode/Subject1Opt1.java
@@ -3,24 +3,23 @@ package com.dap.leecode;
 import java.util.*;
 
 /**
- * @Desc: LeeCode题目1，两数相加
- * 1、hashMap中containsKey也有循环遍历
- *
- * @Author: hepeng
- * @Date: 2019/9/16 22:31
- */
-public class Subject1 {
+  *  @Desc:  LeeCode题目1，两数相加，优化1
+  *  
+  *  @Author:    hepeng
+  *  @Date:  2020/7/18 16:55
+  */
+public class Subject1Opt1 {
 
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5};
-        int[] result = twoSum(nums, 5);
+        int[] result = twoSum(nums, 9);
         System.out.println("result : " + Arrays.toString(result));
     }
 
-     /**
-      *  @Desc: 求数组中任意两个相加为target的数的下标，不包含自身
-      *  
-      */
+    /**
+     *  @Desc: 求数组中任意两个相加为target的数的下标，不包含自身
+     *
+     */
     public static int[] twoSum(int[] nums, int target) {
         // 判空
         int length;
@@ -35,25 +34,17 @@ public class Subject1 {
         }
 
         // 遍历
-        Set<Integer> indexSet = new HashSet<>();
+        int[] results = new int[2];
         for (int i = 0; i < length; i++) {
             int value = target - nums[i];
             if (numCache.containsKey(value)) {
                 if (numCache.get(value) != i) {
-                    indexSet.add(i);
-                    indexSet.add(numCache.get(value));
+                    results[0] = i;
+                    results[1] = numCache.get(value);
                 }
             }
         }
 
-        // 返回结果
-        int[] results = new int[indexSet.size()];
-        int i = 0;
-        for (int result : indexSet) {
-            results[i] = result;
-            i++;
-        }
         return results;
     }
-
 }
