26bef6a89d9807b3ad8853652df596b85bd60215
hepeng <peng6.he@tcl.com>
2022-07-14 18:56:03 +0800

    add: Leetcode 21

---
+++ src/main/java/com/dap/leecode/hot100/L21.java
@@ -0,0 +1,38 @@
+package com.dap.leecode.hot100;
+
+/**
+ * Merge Two Lists
+ */
+public class L21 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * ASC
+     * @param listNode1
+     * @param listNode2
+     * @return
+     */
+    static ListNode mergeTwoLists(ListNode listNode1, ListNode listNode2) {
+        return null;
+    }
+
+    static public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
