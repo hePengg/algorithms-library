005a5417e9df67a1769337397361b7ff68a22bb4
hep <377909@qq.com>
2022-07-16 19:48:58 +0800

    feat: Leetcode 3

---
+++ src/main/java/com/dap/leecode/hot100/L3.java
@@ -0,0 +1,33 @@
+package com.dap.leecode.hot100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L3 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+    static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> strTmp = new HashMap<>();
+        int length = 0;
+
+        for (int end = 0, start = 0; end < s.length(); end++) {
+            Character c = s.charAt(end);
+
+            //
+            if (strTmp.containsKey(c)) {
+                start = Math.max(start, strTmp.get(c) + 1);
+            }
+
+            length = Math.max(length, end - start + 1);
+
+            // key-> char, value -> char last index
+            strTmp.put(c, end);
+        }
+
+        return length;
+    }
+
+}
