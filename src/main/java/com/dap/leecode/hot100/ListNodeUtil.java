2b6af82f97e5f558973731f4fedc92eed3bbf539
hep <377909@qq.com>
2022-07-16 15:38:31 +0800

    perf: 优化链表指针移动

--- src/main/java/com/dap/leecode/hot100/ListNodeUtil.java
+++ src/main/java/com/dap/leecode/hot100/ListNodeUtil.java
@@ -4,18 +4,29 @@ import java.util.Random;
 
 public class ListNodeUtil {
 
-    public static ListNode createAscendingListNode(int length) {
-        ListNode listNode = new ListNode(randomAsc(5));
+    public static ListNode createRandomListNode(int length) {
+        ListNode listNode = new ListNode(new Random().nextInt(10));
         ListNode tmp = listNode;
-        while (length != 0) {
-            tmp.next = new ListNode(randomAsc(tmp.val));
-            tmp = listNode.next;
+        while (length != 1) {
+            tmp.next = new ListNode(new Random().nextInt(10));
+            tmp = tmp.next;
             length--;
         }
         return listNode;
     }
 
-    public static int randomAsc(int val) {
+    public static ListNode createAscendingListNode(int length) {
+        ListNode listNode = new ListNode(randomAsc(5));
+        ListNode tmp = listNode;
+        while (length != 1) {
+            tmp.next = new ListNode(randomAsc(tmp.val));
+            tmp = tmp.next;
+            length--;
+        }
+        return listNode;
+    }
+
+    private static int randomAsc(int val) {
         return new Random().nextInt(100) + val;
     }
 }
