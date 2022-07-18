43e648c88ca605efde1aff4c92d0e073e8c01b1a
hepeng <peng6.he@tcl.com>
2022-07-18 18:43:47 +0800

    test：stack test

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,12 +1,8 @@
-package com.dap.leecode;
-
 import java.util.Stack;
 
-public class Test {
+public class StackTest {
 
     public static void main(String[] args) {
-//        System.out.println(1 << 30);
-
         Stack<Character> stack = new Stack<>();
 
         stack.push('a');
